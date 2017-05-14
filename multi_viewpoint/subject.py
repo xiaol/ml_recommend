5aecca82a6804b5baa0cf7443a9c8a3359207f72
liulei <liulei@lieying.cn>
2017-05-14 21:47:46 +0800

    update

--- multi_viewpoint/subject.py
+++ multi_viewpoint/subject.py
@@ -49,7 +49,7 @@ def create_subject(nids):
 def create_subject_class(sub_id):
     topic_class_url = prefix + '/topic_classes'
     time = datetime.datetime.now()
-    class_name = str(time.month) + '.' + str(time.day)
+    class_name = str(time.month) + '.' + str(time.day) + '.' + str(time.hour) + '.' + str(time.minute)
     #检测是否已经存在
     check_class_ex = "select * from topicclasslist where topic=%s and name=%s"
     conn, cursor = get_postgredb_query()
