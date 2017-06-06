b8b1743cbb00f4d7d39f5f767b98169b861b109d
liulei <liulei@lieying.cn>
2017-06-06 14:28:07 +0800

    update

--- cf_service.py
+++ cf_service.py
@@ -5,10 +5,21 @@
 # @File    : cf_service.py
 # @Software: PyCharm Community Edition
 
+import sys
+import tornado
+from tornado import httpserver
+from tornado import web
+from user_based_cf import data_process
 
 
 
 if __name__ == '__main__':
-    from user_based_cf import data_process
-    while True:
-        data_process.get_user_topic_cf()
+    port = int(sys.argv[1])
+    http_server = tornado.httpserver.HTTPServer(tornado.web.Application())
+    http_server.listen(port)  # 同时提供手工处理端口
+    if port == 9949:
+        while True:
+            data_process.get_user_topic_cf()
+
+
+    tornado.ioloop.IOLoop.instance().start()
