19a23c01349bb9c925ac48635003a8fc6ad93559
liulei <liulei@lieying.cn>
2017-06-09 16:15:01 +0800

    update

--- graphlab_lda/timed_task.py
+++ graphlab_lda/timed_task.py
@@ -8,6 +8,8 @@
 from util.doc_process import get_postgredb
 from redis_process import nid_queue
 from data_process import channel_for_topic_dict
+import datetime
+from datetime import timedelta
 
 #定义取用户点击的循环周期
 period = 3
@@ -15,14 +17,20 @@ period = 3
 click_sql = "select c.uid, c.nid, c.ctime from newsrecommendclick c \
 inner join newslist_v2 nl  on c.nid=nl.nid \
 INNER JOIN channellist_v2 cl on nl.chid = cl.id \
-where cname in ({0}) and c.ctime > now() - INTERVAL '{1} second' "
+where cname in ({0}) and c.ctime > {}"
+#where cname in ({0}) and c.ctime > now() - INTERVAL '{1} second' and c.stime>0"
+
+last_time = datetime.datetime.now() - timedelta(seconds=3)
 
 channels = ', '.join("\'" + ch+"\'" for ch in channel_for_topic_dict.keys())
 def get_clicks_5m():
+    global last_time
     conn, cursor = get_postgredb()
-    cursor.execute(click_sql.format(channels, period))
+    #cursor.execute(click_sql.format(channels, period))
+    cursor.execute(click_sql.format(channels, last_time))
     rows = cursor.fetchall()
     for r in rows:
+        last_time = r[2]
         ctime_str = r[2].strftime('%Y-%m-%d %H:%M:%S')
         nid_queue.produce_user_click_lda(r[0], r[1], ctime_str)
     cursor.close()
