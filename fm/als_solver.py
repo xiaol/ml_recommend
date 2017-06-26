00eeabebccea9f5bfe0d0a30bc089573fbd81efc
xiaol <liuxiao.in@qq.com>
2017-06-26 14:08:42 +0800

    add some fm solvers

---
+++ fm/als_solver.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+
+
+from fastFM.datasets import make_user_item_regression
+from sklearn.cross_validation import train_test_split
+from fastFM import als
+from sklearn.metrics import mean_squared_error
+
+# This sets up a small test dataset.
+X, y, _ = make_user_item_regression(label_stdev=.4)
+X_train, X_test, y_train, y_test = train_test_split(X, y)
+print X.todense()
+print X.data
+print X.indices
+print X.indptr
+
+fm = als.FMRegression(n_iter=1000, init_stdev=0.1, rank=2, l2_reg_w=0.1, l2_reg_V=0.5)
+fm.fit(X_train, y_train)
+y_pred = fm.predict(X_test)
+
+print y_pred
+
+print 'mse:', mean_squared_error(y_test, y_pred)
+
+
+
