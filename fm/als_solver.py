b41ce51c3658d5f760f775d9d3f54cb1c3889fcc
xiaol <liuxiao.in@qq.com>
2017-06-26 17:27:44 +0800

    add evaluator ,data etl methods

--- fm/als_solver.py
+++ fm/als_solver.py
@@ -9,10 +9,6 @@ from sklearn.metrics import mean_squared_error
 # This sets up a small test dataset.
 X, y, _ = make_user_item_regression(label_stdev=.4)
 X_train, X_test, y_train, y_test = train_test_split(X, y)
-print X.todense()
-print X.data
-print X.indices
-print X.indptr
 
 fm = als.FMRegression(n_iter=1000, init_stdev=0.1, rank=2, l2_reg_w=0.1, l2_reg_V=0.5)
 fm.fit(X_train, y_train)
