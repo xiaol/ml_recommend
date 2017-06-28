984da37570a6fae142827b72d1564a3f74f7fb8a
xiaol <liuxiao.in@qq.com>
2017-06-28 15:13:46 +0800

    add brand feature

--- fm/etl_user_data.py
+++ fm/etl_user_data.py
@@ -3,6 +3,7 @@
 from util.postgres import postgres as pg
 import datetime
 
+
 # TODO should cover unread users for cold start
 def get_active_user(time_interval='1 day', click_times=1):
     nt = datetime.datetime.now()
@@ -13,15 +14,15 @@ def get_active_user(time_interval='1 day', click_times=1):
             group by uid HAVING "count"(*)>={}
         '''
     rows = pg.query(sql.format(str_now, time_interval, click_times))
-    active_users = [r[0] for r in rows]
-    return active_users
+    a_users = [r[0] for r in rows]
+    return a_users
 
 
-def enumerate_user_brand():
-    pass
+def enumerate_user_brand(active_users):
+    return enumerate_user_attribute('brand', active_users)
 
 
-def enumerate_user_os_version():
+def enumerate_user_os():
     pass
 
 
@@ -37,17 +38,29 @@ def construct_user_feature_matrix():
 # network int, ctype int, province text, city text, area text
 def enumerate_user_attribute(attribute_name, active_users):
     user_device_sql = '''
-                      select count(distinct {}) 
+                      select distinct {} 
                       from user_device 
                       where uid in ({})
                       '''
     rows = pg.query(user_device_sql.format(attribute_name, ','.join(str(u) for u in active_users)))
-    return rows[0][0]
+    feature_dict = dict((r[0], 0) for r in rows)
+    return feature_dict
 
 
-def load(active_users):
-    return {1: [1, 0], 2: [0, 1], 3: [1, 0], 4: [0, 1]}   # user_id, user_feature vector
+def load(active_users, ):
+    users_feature_dict = {}
+    sql = '''
+           SELECT * from user_device where uid={}
+    '''
+    feature_brand_dict = enumerate_user_brand(active_users)
+    for user in active_users:
+        user_detail = pg.query(sql.format(str(user)))
+        copy_feature_brand_dict = feature_brand_dict.copy()
+        copy_feature_brand_dict[user_detail[3]] = 1
+        users_feature_dict[user] = feature_brand_dict.values()
+    return users_feature_dict
+    # return {1: [1, 0], 2: [0, 1], 3: [1, 0], 4: [0, 1]}   # user_id, user_feature vector
 
 if __name__ == '__main__':
-    active_users = get_active_user()
-    print enumerate_user_brand()
+    users = get_active_user(time_interval='30 minutes')
+    print enumerate_user_brand(users)
