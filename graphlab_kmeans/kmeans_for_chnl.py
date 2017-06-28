0d8509aa68da0f2ff7e1ab8736e482e6a0a13507
liulei <liulei@lieying.cn>
2017-06-28 12:41:12 +0800

    update

--- graphlab_kmeans/kmeans_for_chnl.py
+++ graphlab_kmeans/kmeans_for_chnl.py
@@ -26,10 +26,12 @@ def predict_chnl_news(chnl_name, num_limit=None):
     if num_limit:
         chnl_sql = '''select nid from info_news a inner join channellist_v2 cl
                   on a.chid=cl.id where cl.cname={} limit {}'''
+        logger_chnl.info(cursor.mogrify(chnl_sql.format(chnl_name)))
         cursor.execute(chnl_sql.format(chnl_name, num_limit))
     else:
         chnl_sql = '''select nid from info_news a inner join channellist_v2 cl
                   on a.chid=cl.id where cl.cname={}'''
+        logger_chnl.info(cursor.mogrify(chnl_sql.format(chnl_name)))
         cursor.execute(chnl_sql.format(chnl_name))
 
     rows = cursor.fetchall()
