350edce94bd5462841d2c819fb538339bfabe859
liulei <liulei@lieying.cn>
2017-07-04 16:24:54 +0800

    update

--- sim_hash/sim_hash.py
+++ sim_hash/sim_hash.py
@@ -125,6 +125,7 @@ def del_same_old_news(nid, nid_hash_dict):
             #print t0
             #print '    6'
             cursor.execute(insert_same_sql.format(nid, n, diff_bit, t0.strftime('%Y-%m-%d %H:%M:%S'), offnid)) #记录去重操作
+            logger.inf('________ delete {}'.format(offnid))
             nid_hash_dict.pop(offnid)
             if offnid == nid: #新检测的新闻下线,则不再需要对比其他
                 break
