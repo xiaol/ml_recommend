2aa20bc0839f2ac4fc8c683bc310564586391de3
xiaol <liuxiao.in@qq.com>
2017-07-24 14:21:19 +0800

    fixed numeric bugs

--- fm/model.py
+++ fm/model.py
@@ -183,7 +183,7 @@ def update_positive_sample_feature(click_samples_list, samples_feature_dict,
 
         feature_key = tuple(feature_list)
         if click_sample[3] != 0 and click_sample[3] < 200:
-            score = min(click_sample[3], 20)/20 * 5
+            score = min(click_sample[3], 30)/30.0 * 10
             samples_feature_dict[feature_key] = score
         else:
             samples_feature_dict[feature_key] = 1
