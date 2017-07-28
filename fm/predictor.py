bde73a39af11752f6ab5cbd2141abdf37b6ded73
xiaol <liuxiao.in@qq.com>
2017-07-28 10:31:01 +0800

    update model

--- fm/predictor.py
+++ fm/predictor.py
@@ -74,7 +74,7 @@ def predict(time_interval='10 seconds', user=-1):
     recommend_items_list = []
     for i in range(len(sorted_list)):
         recommend_items_list.append(candidates_dict[sorted_list[i][0]])
-    update_user_ranking_recommend(user, recommend_items_list[:200])
+    update_user_ranking_recommend(user, recommend_items_list[:500])
 
 
 if __name__ == '__main__':
